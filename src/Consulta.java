4d6c7a4838043f0943275f1813f1ef735c800f40
Diinz <joseacoca19@gmail.com>
2018-02-20 15:46:31 +0100

    Hemos creado el recurso para el pool de conextiones

---
+++ src/Consulta.java
@@ -0,0 +1,108 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+@WebServlet("/consulta")
+public class Consulta extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Consulta() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Recogemos el parámetro "autor" del formulario.
+		
+		String autor = request.getParameter("autor");
+		
+		// Preparamos la salida del servidor.
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		// Objetos para la conexión y consulta a la base de datos.
+		Connection con = null;
+		PreparedStatement stmt = null;
+	
+		// Cargar el driver de MySQL
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// Credenciales para la base de datos.
+			String url = "jdbc:mysql://localhost/TiendaLibros";
+			String usuario = "Librero";
+			String password  = "Ageofempires2";
+			
+			// Ejecutamos una consulta SQL.
+			con = DriverManager.getConnection(url, usuario, password);
+			
+			String sql = "select * from libros where autor = ?";
+			stmt = con.prepareStatement(sql);
+			
+			stmt.setString(1, autor);
+			
+			ResultSet resultados = stmt.executeQuery();
+			// Generamos la página HTML de salida
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<meta charset='utf-8'>");
+			out.println("<title>Consulta</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>Consulta por autor</h1>");
+			out.println("<p>Los libros publicados por " + autor + "son:</p>");
+			// Vamos recorriendo el ResultSet y mostrando sus campos.
+			while (resultados.next()) {
+			out.println("<hr>");
+			out.println("Título: " + resultados.getString("titulo")
+			+ "<br>");
+			out.println("Precio: " + resultados.getString("precio")
+			+ "<br>");
+			out.println("Cantidad: " +
+			resultados.getString("cantidad")
+			+ "<br>");
+			}
+			out.println("</body>");
+			out.println("</html>");
+			
+			
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("ERROR AL ACCEDER A LA BASE DE DATOS");
+			e.printStackTrace();
+			} finally {
+			try {
+			stmt.close();
+			con.close();
+			
+			} catch (SQLException e) {
+			System.out
+			.println("ERROR AL CERRAR LOS OBJETOS DE LA BASE DE DATOS");
+			
+			e.printStackTrace();
+			}
+			}
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
