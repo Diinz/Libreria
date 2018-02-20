3c70b9df82d23a49483af645be73ec0caeecef7f
Diinz <joseacoca19@gmail.com>
2018-02-20 16:24:34 +0100

    La conexión a ala base de datos utiliza ahora un pool

--- src/Consulta.java
+++ src/Consulta.java
@@ -1,63 +1,77 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
-
+import javax.sql.DataSource;
 
 @WebServlet("/consulta")
 public class Consulta extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-
-    public Consulta() {
-        super();
-    }
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	DataSource pool;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
 		
+		try {
+			InitialContext contexto = new InitialContext();
+			
+			pool = (DataSource) contexto.lookup("java:comp/env/jdbc/mysql_tiendalibros");
+			
+			if (pool == null) {
+				throw new ServletException("ERROR AL ACCEDER AL POOL DE CONEXIONES(NULL)");
+			}
+			
+		} catch (NamingException e) {
+		
+			System.out.println("ERROR AL ACCEDER AL POOL DE CONEXIONES");
+			e.printStackTrace();
+		}
+	}
+
+	public Consulta() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		// Recogemos el parámetro "autor" del formulario.
-		
+
 		String autor = request.getParameter("autor");
-		
+
 		// Preparamos la salida del servidor.
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
+
 		// Objetos para la conexión y consulta a la base de datos.
 		Connection con = null;
 		PreparedStatement stmt = null;
-	
+
 		// Cargar el driver de MySQL
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			
-			// Credenciales para la base de datos.
-			String url = "jdbc:mysql://localhost/TiendaLibros";
-			String usuario = "Librero";
-			String password  = "Ageofempires2";
-			
+
 			// Ejecutamos una consulta SQL.
-			con = DriverManager.getConnection(url, usuario, password);
-			
+			con = pool.getConnection();
+
 			String sql = "select * from libros where autor = ?";
 			stmt = con.prepareStatement(sql);
-			
+
 			stmt.setString(1, autor);
-			
+
 			ResultSet resultados = stmt.executeQuery();
 			// Generamos la página HTML de salida
 			out.println("<html>");
@@ -70,39 +84,39 @@ public class Consulta extends HttpServlet {
 			out.println("<p>Los libros publicados por " + autor + "son:</p>");
 			// Vamos recorriendo el ResultSet y mostrando sus campos.
 			while (resultados.next()) {
-			out.println("<hr>");
-			out.println("Título: " + resultados.getString("titulo")
-			+ "<br>");
-			out.println("Precio: " + resultados.getString("precio")
-			+ "<br>");
-			out.println("Cantidad: " +
-			resultados.getString("cantidad")
-			+ "<br>");
+				out.println("<hr>");
+				out.println("Título: " + resultados.getString("titulo")
+						+ "<br>");
+				out.println("Precio: " + resultados.getString("precio")
+						+ "<br>");
+				out.println("Cantidad: " + resultados.getString("cantidad")
+						+ "<br>");
 			}
 			out.println("</body>");
 			out.println("</html>");
-			
-			
-			
-			
+
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println("ERROR AL ACCEDER A LA BASE DE DATOS");
 			e.printStackTrace();
-			} finally {
+		} finally {
 			try {
-			stmt.close();
-			con.close();
-			
+				stmt.close();
+				con.close();
+
 			} catch (SQLException e) {
-			System.out
-			.println("ERROR AL CERRAR LOS OBJETOS DE LA BASE DE DATOS");
-			
-			e.printStackTrace();
+				System.out
+						.println("ERROR AL CERRAR LOS OBJETOS DE LA BASE DE DATOS");
+
+				e.printStackTrace();
 			}
-			}
+		}
 
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
 	}
 
 }
